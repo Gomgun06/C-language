4ed18cc9523f238578a5bcc03475b0c8400668e2
Gomgun06 <115025917+Gomgun06@users.noreply.github.com>
2022-10-06 03:29:22 +0900

    Create 사운드바 아래로.c

---
+++ 사운드바 아래로.c
@@ -0,0 +1,16 @@
+srand((unsigned)time(NULL));
+	int k, jul, met, sound[60];
+
+	for (k = 0; k < 200; k++) {
+		system("cls");
+		for (met = 0; met < 60; met++)
+			sound[met] = rand() % 25;
+
+		for (jul = 0; jul < 25; jul++) {
+			for (met = 0; met < 60; met++) {
+				if (sound[met] >= jul) printf("ㅁ");
+				else printf("  ");
+			}
+			printf("\n");
+		}
+	}
